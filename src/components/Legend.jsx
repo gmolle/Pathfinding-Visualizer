2b122090364f1bbf7c322a97c2fe6f6f8e46e652
Garrett M <molle_g@yahoo.com>
2025-05-20 02:55:24 -0400

    Added icons for certain nodes, and updated the legend accordingly

--- src/components/Legend.jsx
+++ src/components/Legend.jsx
@@ -1,30 +1,38 @@
+import { CircleArrowRight, Target, Weight } from "lucide-react";
+
 // Legend.jsx
 export default function Legend() {
   return (
-    <div className="flex gap-4 mb-4">
+    <div className="flex gap-6 mb-4">
       <div className="flex items-center gap-2">
-        <div className="w-4 h-4 bg-green-500 border border-gray-300"></div>
+        <CircleArrowRight size={30} className="text-green-300" />
         <span>Start</span>
       </div>
       <div className="flex items-center gap-2">
-        <div className="w-4 h-4 bg-red-500 border border-gray-300"></div>
+        <Target size={30} className="text-red-500" />
         <span>End</span>
       </div>
       <div className="flex items-center gap-2">
-        <div className="w-4 h-4 bg-gray-900 border border-gray-300"></div>
+        <div className="w-6 h-6 bg-gray-900"></div>
         <span>Wall</span>
       </div>
       <div className="flex items-center gap-2">
-        <div className="w-4 h-4 bg-gray-500 border border-gray-300"></div>
+        <div className="flex items-center justify-center w-6 h-6 bg-indigo-200 ">
+          <Weight size={20} className="text-yellow-700" />
+        </div>
         <span>Weighted Node</span>
       </div>
       <div className="flex items-center gap-2">
-        <div className="w-4 h-4 bg-blue-200 border border-gray-300"></div>
-        <div className="w-4 h-4 bg-purple-400 border border-gray-300"></div>
+        <div className="w-6 h-6 bg-white border border-blue-300"></div>
+        <span>Unvisted</span>
+      </div>
+      <div className="flex items-center gap-2">
+        <div className="w-6 h-6 bg-blue-200 "></div>
+        <div className="w-6 h-6 bg-purple-400  "></div>
         <span>Visited</span>
       </div>
       <div className="flex items-center gap-2">
-        <div className="w-4 h-4 bg-yellow-300 border border-gray-300"></div>
+        <div className="w-6 h-6 bg-yellow-300 "></div>
         <span>Path</span>
       </div>
     </div>
--- src/components/Node.jsx
+++ src/components/Node.jsx
@@ -1,3 +1,5 @@
+import { CircleArrowRight, Target, Weight } from "lucide-react";
+
 export default function Node({ node, onMouseDown, onMouseEnter, onMouseUp }) {
   const {
     row,
@@ -11,12 +13,9 @@ export default function Node({ node, onMouseDown, onMouseEnter, onMouseUp }) {
     isCurrent,
     animationKey,
   } = node;
+
   const bgColor = isCurrent
     ? "bg-purple-400"
-    : isStart
-    ? "bg-green-500"
-    : isEnd
-    ? "bg-red-500"
     : isWall
     ? "bg-gray-900"
     : isPath
@@ -24,8 +23,9 @@ export default function Node({ node, onMouseDown, onMouseEnter, onMouseUp }) {
     : isVisited
     ? "bg-cyan-300"
     : weight === 2
-    ? "bg-gray-400"
+    ? "bg-indigo-200"
     : "bg-white";
+
   const cursor = isStart || isEnd ? "cursor-grab" : "cursor-default";
   const borderClass =
     isWall || isPath ? "" : "border-r border-b border-blue-300";
@@ -37,12 +37,24 @@ export default function Node({ node, onMouseDown, onMouseEnter, onMouseUp }) {
 
   return (
     <div
-      className={`node w-6 h-6 ${bgColor} ${cursor} ${borderClass} ${animationClass}`}
+      className={`node w-6 h-6 ${bgColor} ${cursor} ${borderClass} ${animationClass} relative`}
       data-weight={weight}
       data-animation-key={animationKey}
       onMouseDown={() => onMouseDown(row, col)}
       onMouseEnter={() => onMouseEnter(row, col)}
       onMouseUp={() => onMouseUp(row, col)}
-    />
+    >
+      <div className="absolute inset-0 flex items-center justify-center pointer-events-none">
+        {isEnd ? (
+          <Target size={20} className="text-red-500" />
+        ) : isStart ? (
+          <CircleArrowRight size={50} className="text-green-300" />
+        ) : weight > 1 ? (
+          <Weight size={20} className="text-yellow-700" />
+        ) : (
+          ""
+        )}
+      </div>
+    </div>
   );
 }
